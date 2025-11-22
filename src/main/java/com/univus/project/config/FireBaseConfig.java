44cb08397fb4f6d918d00d9f8e30e2a3586dd318
hakyungMin <hkmin999@gmail.com>
2025-11-22 15:04:07 +0900

    11.22 firebase 수정

--- src/main/java/com/univus/project/config/FireBaseConfig.java
+++ src/main/java/com/univus/project/config/FireBaseConfig.java
@@ -1,42 +1,15 @@
-package com.univus.project.config;
+//// firebase.js
+//import { initializeApp } from "firebase/app";
+//import { getStorage } from "firebase/storage";
 //
-//import com.google.auth.oauth2.GoogleCredentials;
-//import com.google.firebase.FirebaseApp;
-//import com.google.firebase.FirebaseOptions;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.context.annotation.Configuration;
+//        const firebaseConfig = {
+//apiKey: "YOUR_API_KEY",
+//authDomain: "PROJECT_ID.firebaseapp.com",
+//projectId: "PROJECT_ID",
+//storageBucket: "PROJECT_ID.appspot.com",
+//messagingSenderId: "SENDER_ID",
+//appId: "APP_ID",
+//        };
 //
-//
-//import javax.annotation.PostConstruct;
-//import java.io.FileInputStream;
-//
-//
-//@Slf4j
-//@Configuration
-//public class FirebaseConfig {
-//
-//
-//    @PostConstruct
-//    public void init() {
-//        try {
-//            FileInputStream serviceAccount = new FileInputStream("src/main/resources/firebase-service-key.json");
-//
-//
-//            FirebaseOptions options = FirebaseOptions.builder()
-//                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-//                    .setStorageBucket("YOUR_BUCKET_NAME.appspot.com")
-//                    .build();
-//
-//
-//            if (FirebaseApp.getApps().isEmpty()) {
-//                FirebaseApp.initializeApp(options);
-//            }
-//
-//
-//            log.info("Firebase initialized");
-//        } catch (Exception e) {
-//            log.error("Firebase init error: {}", e.getMessage());
-//        }
-//    }
-//}
-//YOUR_BUCKET_NAME = Firebase Storage 버킷 이름
+//        const app = initializeApp(firebaseConfig);
+//export const storage = getStorage(app);
