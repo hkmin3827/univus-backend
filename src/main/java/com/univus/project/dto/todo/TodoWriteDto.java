7409265004aa7fb70a21334bd4eb4465a651deb4
신아영 <shinahyeong@sin-ayeong-ui-MacBookAir.local>
2025-12-01 05:09:37 +0900

    todo

--- src/main/java/com/univus/project/dto/todo/TodoWriteDto.java
+++ src/main/java/com/univus/project/dto/todo/TodoWriteDto.java
@@ -8,9 +8,10 @@ import lombok.ToString;
 
 @Getter @Setter @NoArgsConstructor @ToString
 public class TodoWriteDto {
-    private String email;   // 작성자 이메일
+    @JsonProperty("teamId")
     private Long teamId;    // 할일이 속한 팀
     @JsonProperty("boardId")
     private Long boardId;   // 할일이 속한 게시판
+    @JsonProperty("content")
     private String content; // 할일 내용
 }
